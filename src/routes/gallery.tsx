10b6cfef74d28abb7faaf46d4cdc9d1d3a7052b6
Ryan Wade <ryanwade628@gmail.com>
2023-11-17 08:32:41 -0500

    Clean up gallery route

--- src/routes/gallery.tsx
+++ src/routes/gallery.tsx
@@ -1,79 +1,80 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import cats from "../utils/cats";
+import AspectRatio from '@mui/joy/AspectRatio';
 import Box from "@mui/joy/Box";
 import Typography from '@mui/joy/Typography';
 import { Masonry } from "masonic";
 
 import ImageIcon from '@mui/icons-material/Image';
 
-
 export default function Gallery() {
-  const [items, setItems] = React.useState(getFakeItems);
+  const [items, setItems] = useState(getFakeItems);
   const { page = "cats" } = useParams();
-  const otherPage = page === "cats" ? "kittens" : "cats";
 
-  React.useEffect(() => {
+  useEffect(() => {
     setItems(getFakeItems());
   }, [page]);
 
   return (
-    <div>
-      <div>
-        <span>filter by:</span>
-        <h2>{page}</h2>
-        <Link to={`/${otherPage}`}>{otherPage}</Link>
-      </div>
-      <Masonry
-        // When switching between pages that use the same
-        // Route component, using the `key` prop is the
-        // best way to create a new layout with your
-        // updated items
-        key={page}
-        items={items}
-        columnGutter={8}
-        columnWidth={172}
-        overscanBy={6}
-        render={GalleryCard}
-      />
-    </div>
+    <Masonry
+      key={page}
+      items={items}
+      columnGutter={8}
+      columnCount={3}
+      overscanBy={2}
+      render={GalleryCard}
+    />
   );
 };
 
-const GalleryCard = ({ data: { id, name, src, width, height } }) => (
-  <Box>
-    <img
-      style={{ display: "block", width: "100%" }}
-      src={
-        src
-      }
-      alt="kitty"
-    />
-    <Box sx={{
-      position: 'absolute',
-      width: '100%',
-      bottom: 0,
-      background: "rgba(0, 0, 0, 0.5)",
-      textAlign: "center",
-    }}>
-      <Typography
-        startDecorator={<ImageIcon fontSize="lg" />}
-        justifyContent="center"
-        textColor="#f1f1f1"
-        variant="plain"
-        level="body-lg"
-      >
-        {name}
-      </Typography>
-    </Box>
-  </Box >
-)
+const GalleryCard = ({ data: { id, name, src, width, height } }: { data: ItemProps }) => {
+  return (
+    <Link to={`/${id}`}>
+      <AspectRatio variant="plain" ratio={`${width}/${height}`} >
+        <img
+          style={{ display: "block", width: "100%" }}
+          src={src}
+          alt="kitty"
+        />
+      </AspectRatio>
+      <Box sx={{
+        position: 'absolute',
+        width: '100%',
+        bottom: 0,
+        background: "rgba(0, 0, 0, 0.5)",
+        textAlign: "center",
+      }}>
+        <Typography
+          startDecorator={<ImageIcon fontSize="inherit" />}
+          justifyContent="center"
+          textColor="#f1f1f1"
+          variant="plain"
+          level="body-lg"
+        >
+          {name}
+        </Typography>
+      </Box>
+    </Link>
+  )
+}
 
-const randomChoice = items => items[Math.floor(Math.random() * items.length)];
+interface ItemProps {
+  id: number,
+  name: string,
+  src: string,
+  width: number,
+  height: number,
+}
+
+const randomChoice = (items: string[]) => items[Math.floor(Math.random() * items.length)];
+const generateRandomString = (length = 6) => Math.random().toString(20).substr(2, length)
 const getFakeItems = (cur = 0) => {
-  const fakeItems = [];
+  const fakeItems: ItemProps[] = [];
   for (let i = 5000 * cur; i < cur * 5000 + 5000; i++)
-    fakeItems.push({ id: i, name: "someName", src: randomChoice(cats), width: 16, height: 9 });
+    fakeItems.push({
+      id: i, name: generateRandomString(), src: randomChoice(cats), width: 16, height: 9
+    })
   return fakeItems;
 };
 
