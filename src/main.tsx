3d1ecabb31dae5acbed0dfa5e31bb2d4ec2c1b7f
Ryan Wade <ryanwade628@gmail.com>
2023-11-23 21:02:26 -0500

    Add loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import '@fontsource/inter';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "@fontsource/inter";
 
-import Root from './routes/root'
-import Gallery from './routes/gallery'
+import { useGalleryStore } from "./state/gallery";
+
+import Root from "./routes/root";
+import Gallery from "./routes/gallery";
+import Lightbox from "./routes/lightbox";
+
+const loader = async () => {
+  const existingGalleries = useGalleryStore.getState().galleries;
+  if (existingGalleries.length === 0) {
+    const res = await fetch(`api/galleries`);
+    if (res.status === 404) {
+      throw new Response("Not Found", { status: 404 });
+    }
+    const galleries = await res.json();
+    useGalleryStore.setState({ galleries: galleries });
+    return galleries;
+  }
+  return existingGalleries;
+};
 
 const router = createBrowserRouter([
   {
@@ -16,15 +30,21 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/:page?",
-        element: <Gallery />
-      }
-    ]
+        element: <Gallery />,
+        loader: loader,
+        children: [
+          {
+            path: "modal/:index",
+            element: <Lightbox />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
