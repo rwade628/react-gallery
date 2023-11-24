e8af57f7dce528f952164b006dfde99d0acc95ae
Ryan Wade <ryanwade628@gmail.com>
2023-11-23 21:03:40 -0500

    Pull data from loader and prettier formatting

--- src/routes/gallery.tsx
+++ src/routes/gallery.tsx
@@ -1,52 +1,78 @@
-import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import cats from "../utils/cats";
-import AspectRatio from '@mui/joy/AspectRatio';
+import { useEffect } from "react";
+import { Link, useParams, useLoaderData } from "react-router-dom";
+import AspectRatio from "@mui/joy/AspectRatio";
 import Box from "@mui/joy/Box";
-import Typography from '@mui/joy/Typography';
+import Typography from "@mui/joy/Typography";
 import { Masonry } from "masonic";
 
-import ImageIcon from '@mui/icons-material/Image';
+import ImageIcon from "@mui/icons-material/Image";
+import MovieIcon from "@mui/icons-material/Movie";
+
+import { Gallery as GalleryType } from "../state/gallery";
 
 export default function Gallery() {
-  const [items, setItems] = useState(getFakeItems);
-  const { page = "cats" } = useParams();
+  const data = useLoaderData() as GalleryType[];
+  let items = data;
+  const { page = 0 } = useParams();
 
-  useEffect(() => {
-    setItems(getFakeItems());
-  }, [page]);
+  useEffect(() => {});
+
+  if (page != 0) {
+    const gallery = data.find((gallery) => gallery.id == page);
+    if (gallery && gallery.photos) {
+      items = gallery.photos;
+    }
+  }
 
   return (
     <Masonry
       key={page}
       items={items}
       columnGutter={8}
-      columnCount={3}
+      columnWidth={350}
       overscanBy={2}
       render={GalleryCard}
     />
   );
-};
+}
 
-const GalleryCard = ({ data: { id, name, src, width, height } }: { data: ItemProps }) => {
+const GalleryCard = ({
+  index,
+  data: { id, name, width, height, src, thumb, type },
+}: {
+  index: number;
+  data: GalleryType;
+}) => {
+  let linkTo = `/${id}`;
+  if (!type || type === "movie") {
+    linkTo = `modal/${index}`;
+  }
   return (
-    <Link to={`/${id}`}>
-      <AspectRatio variant="plain" ratio={`${width}/${height}`} >
+    <Link to={linkTo}>
+      <AspectRatio variant="plain" ratio={`${width}/${height}`}>
         <img
           style={{ display: "block", width: "100%" }}
-          src={src}
+          src={thumb || src}
           alt="kitty"
         />
       </AspectRatio>
-      <Box sx={{
-        position: 'absolute',
-        width: '100%',
-        bottom: 0,
-        background: "rgba(0, 0, 0, 0.5)",
-        textAlign: "center",
-      }}>
+      <Box
+        sx={{
+          position: "absolute",
+          width: "100%",
+          bottom: 0,
+          background: "rgba(0, 0, 0, 0.5)",
+          textAlign: "center",
+        }}
+      >
         <Typography
-          startDecorator={<ImageIcon fontSize="inherit" />}
+          startDecorator={
+            type === "photo" ? (
+              <ImageIcon fontSize="inherit" />
+            ) : (
+              <MovieIcon fontSize="inherit" />
+            )
+          }
           justifyContent="center"
           textColor="#f1f1f1"
           variant="plain"
@@ -56,25 +82,5 @@ const GalleryCard = ({ data: { id, name, src, width, height } }: { data: ItemPro
         </Typography>
       </Box>
     </Link>
-  )
-}
-
-interface ItemProps {
-  id: number,
-  name: string,
-  src: string,
-  width: number,
-  height: number,
-}
-
-const randomChoice = (items: string[]) => items[Math.floor(Math.random() * items.length)];
-const generateRandomString = (length = 6) => Math.random().toString(20).substr(2, length)
-const getFakeItems = (cur = 0) => {
-  const fakeItems: ItemProps[] = [];
-  for (let i = 5000 * cur; i < cur * 5000 + 5000; i++)
-    fakeItems.push({
-      id: i, name: generateRandomString(), src: randomChoice(cats), width: 16, height: 9
-    })
-  return fakeItems;
+  );
 };
-
